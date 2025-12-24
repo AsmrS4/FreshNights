c145deb41ee5a799a076f93a673e0d1ca977c880
Aymir Sunchugashev <as4n.tsu@gmail.com>
2025-12-25 03:14:11 +0700

    Add signatures for booking controller

--- pet/src/main/java/com/project/pet/controllers/BookingController.java
+++ pet/src/main/java/com/project/pet/controllers/BookingController.java
@@ -1,4 +1,35 @@
 package com.project.pet.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/booking")
 public class BookingController {
+    @PostMapping("/{eventId}")
+    public ResponseEntity<?> createBookingOnEvent(@PathVariable UUID eventId) {
+        return ResponseEntity.ok(null);
+    }
+    @GetMapping
+    //TODO: добавить фильтрацию
+    public ResponseEntity<?> retrieveAllBookings() {
+        return ResponseEntity.ok(null);
+    }
+
+    @DeleteMapping("/{bookingId}")
+    public ResponseEntity<?> cancelBookingOnEvent(@PathVariable UUID bookingId) {
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/{eventId}/guests")
+    public ResponseEntity<?> retrieveGuestsOnEvent(@PathVariable UUID eventId) {
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/{eventId}/stat")
+    public ResponseEntity<?> retrieveEventBookingStat(@PathVariable UUID eventId) {
+        return ResponseEntity.ok(null);
+    }
 }
